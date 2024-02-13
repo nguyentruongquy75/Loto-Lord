b61e39af3eb14a366560b0fd0749fa3bcc8ae5ec
precious75 <5701@generative.xyz>
2024-02-13 21:16:01 +0700

    fix some bug

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ let isPlaying = false;
 let numberSounds = [];
 let waitingNumber = 6000;
 let timeout = null;
-const numbers = [];
+let numbers = [];
 const beat = document.querySelector(".beat");
 const startBtn = document.querySelector(".start");
 const stopBtn = document.querySelector(".stop");
+const winBtn = document.querySelector(".win");
+const resetBtn = document.querySelector(".reset");
 const numberDiv = document.querySelector(".numberDiv");
 const numberContainer = document.querySelector(".multiple-items");
+const checkWinInput = document.querySelector("#checkWin");
+const checkWinBtn = document.querySelector("#checkWinBtn");
+const winInputOverlay = document.querySelector(".overlay");
 
 function addShowcase(number) {
   $(".multiple-items").slick("slickAdd", `<div class="number">${number}</div>`);
@@ -25,18 +30,20 @@ function randomNumber(min, max) {
 }
 
 function playNumber() {
-  timeout = setTimeout(() => {
-    let rdNumber;
-    do {
-      rdNumber = randomNumber(1, 90);
-    } while (numbers.includes(rdNumber));
-    numbers.push(rdNumber);
-    displayNumber(rdNumber);
-    addShowcase(rdNumber);
-    readNumber(rdNumber);
+  if (numbers.length < 90) {
+    timeout = setTimeout(() => {
+      let rdNumber;
+      do {
+        rdNumber = randomNumber(1, 90);
+      } while (numbers.includes(rdNumber));
+      numbers.push(rdNumber);
+      displayNumber(rdNumber);
+      addShowcase(rdNumber);
+      readNumber(rdNumber);
 
-    playNumber();
-  }, waitingNumber);
+      playNumber();
+    }, waitingNumber);
+  }
 }
 
 function stopNumber() {
@@ -54,20 +61,47 @@ function displayNumber(number) {
   numberDiv.textContent = number;
 }
 
+function hideStartButton() {
+  startBtn.classList.add("hide");
+}
+
+function showStartButton() {
+  startBtn.classList.remove("hide");
+}
+
+function hideStopButton() {
+  stopBtn.classList.add("hide");
+}
+
+function showStopButton() {
+  stopBtn.classList.remove("hide");
+}
+
+function hideWinButton() {
+  winBtn.classList.add("hide");
+}
+
+function showWinButton() {
+  winBtn.classList.remove("hide");
+}
+
+function showCheckWinOverlay() {
+  winInputOverlay.classList.remove("hide");
+}
+
+function hideCheckWinOverlay() {
+  winInputOverlay.classList.add("hide");
+}
+
 function startBeat() {
   isPlaying = true;
-  isPlaying && (startBtn.textContent = "Chơi lại");
-
   beat.currentTime = currentTime;
   beat.volume = 0.2;
   beat.play();
-
-  playNumber();
 }
 
 function stopBeat() {
   isPlaying = false;
-  startBtn.textContent = "Tiếp tục";
   currentTime = beat.currentTime;
   beat.pause();
 }
@@ -75,12 +109,71 @@ function stopBeat() {
 function reset() {
   currentTime = 0;
   numbers = [];
+
+  numberDiv.textContent = "...";
+  showStopButton();
+  hideCheckWinOverlay();
+  hideStartButton();
+  startBeat();
+  playNumber();
+  $(".multiple-items").slick("slickRemove", null, null, true);
 }
 
-startBtn.addEventListener("click", startBeat);
+function check() {
+  const numberArr = checkWinInput.value.split(",").map((t) => +t);
+  console.log(numbers);
+  return numberArr.every((v) => numbers.includes(v));
+}
+
+startBtn.addEventListener("click", () => {
+  hideStartButton();
+  startBeat();
+  playNumber();
+  showStopButton();
+  showWinButton();
+});
 stopBtn.addEventListener("click", () => {
+  startBtn.textContent = "Tiếp tục";
+  showStartButton();
   stopBeat();
   stopNumber();
+  hideStopButton();
 });
 
-importSounds();
+winBtn.addEventListener("click", () => {
+  startBtn.textContent = "Tiếp tục";
+  showStartButton();
+  stopBeat();
+  stopNumber();
+  showCheckWinOverlay();
+  hideStopButton();
+});
+
+checkWinBtn.addEventListener("click", () => {
+  const isValid = check();
+  const checkText = document.querySelector(".checkText");
+
+  const TEXT = isValid ? "Chúc mừng bạn yêu" : "Ê ê, báo động giả nhen";
+  checkText.textContent = TEXT;
+  checkText.className = `checkText ${isValid ? "success" : "error"}`;
+});
+
+resetBtn.addEventListener("click", () => {
+  reset();
+});
+
+winInputOverlay.addEventListener("click", (e) => {
+  const winInput = document.querySelector(".winInput");
+  if (!winInput.contains(e.target) && !resetBtn.contains(e.target)) {
+    hideCheckWinOverlay();
+  }
+});
+
+function init() {
+  importSounds();
+  hideStopButton();
+  hideWinButton();
+  hideCheckWinOverlay();
+}
+
+init();
